a67de07a2d8e0880aac152d61af9887b759a4a9a
Artmzyyy <mfaizin648@gmail.com>
2025-07-09 00:07:34 +0700

    Cobain

--- profile.php
+++ profile.php
@@ -242,7 +242,7 @@
         </div>
 
         <div class="developer-social">
-          <a href="https://www.youtube.com/@kraxy4651" target="_blank" class="social-link">
+          <a href="https://www.youtube.com/Bahrii" target="_blank" class="social-link">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <g fill="none" fill-rule="evenodd">
                 <path
